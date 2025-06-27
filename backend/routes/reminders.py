08195fd6812f3ed047727f16297dcde57fa1999a
cate henu <catherine.henu@student.moringaschool.com>
2025-06-27 16:36:52 +0300

    added files

---
+++ backend/routes/reminders.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.main import db
+from ..models.reminder import Reminder
+from ..models.plant import Plant
+from datetime import datetime
+
+reminders_bp = Blueprint('reminders', __name__)
+
+@reminders_bp.route('', methods=['GET'])
+@jwt_required()
+def get_reminders():
+    user_id = get_jwt_identity()
+    reminders = Reminder.query.join(Plant).filter(Plant.user_id == user_id).all()
+    return jsonify([{
+        'id': r.id,
+        'task': r.task,
+        'due_date': r.due_date.isoformat(),
+        'plant_id': r.plant_id
+    } for r in reminders])
+
+@reminders_bp.route('', methods=['POST'])
+@jwt_required()
+def add_reminder():
+    data = request.get_json()
+    plant = Plant.query.filter_by(id=data['plant_id'], user_id=get_jwt_identity()).first_or_404()
+    reminder = Reminder(
+        task=data['task'],
+        due_date=datetime.fromisoformat(data['due_date']),
+        plant_id=plant.id
+    )
+    db.session.add(reminder)
+    db.session.commit()
+    return jsonify({'id': reminder.id}), 201
+
+@reminders_bp.route('/<int:id>', methods=['PUT', 'DELETE'])
+@jwt_required()
+def update_or_delete_reminder(id):
+    user_id = get_jwt_identity()
+    reminder = Reminder.query.join(Plant).filter(Reminder.id == id, Plant.user_id == user_id).first_or_404()
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        reminder.task = data['task']
+        reminder.due_date = datetime.fromisoformat(data['due_date'])
+        db.session.commit()
+        return jsonify({'message': 'Reminder updated'})
+
+    if request.method == 'DELETE':
+        db.session.delete(reminder)
+        db.session.commit()
+        return jsonify({'message': 'Reminder deleted'})
