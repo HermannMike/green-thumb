bf632e064cc882d539f27426bba36392e44fb529
sudeis <ahmedsudeis24@gmail.com>
2025-07-01 15:23:08 +0300

    enable support for credentials in CORS configuration

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ from flask_cors import CORS
 
 cors = CORS()
 
-cors.init_app(app, resources={r"/*": {"origins": app.config['CORS_ORIGINS']}})
+cors.init_app(app, resources={r"/*": {"origins": app.config['CORS_ORIGINS']}}, supports_credentials=True)
 
 import logging
 import traceback
