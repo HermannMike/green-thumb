b4c7130c7216a17515d3144dfd29cd4f351c5653
sudeis <ahmedsudeis24@gmail.com>
2025-07-02 19:24:38 +0300

    fixed endpoint

--- backend/app.py
+++ backend/app.py
@@ -4,17 +4,13 @@ from extensions import db, bcrypt, jwt, cors
 app = Flask(__name__, static_url_path='', static_folder='static')
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://green_thumb_user:StHbRSOhRLCjYqoo5z94vaB1bBjb6tQI@dpg-d1ikcoer433s73aq1a2g-a/green_thumb')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///green_thumb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'your_jwt_secret'
 
 app.config['CORS_ORIGINS'] = [
     'https://green-thumb13.vercel.app',
-    'https://green-thumb13-fps41zcah-sudi67s-projects.vercel.app',
-    'https://green-thumb13-2v43t9nna-sudi67s-projects.vercel.app',
-    'https://green-thumb-1xau.onrender.com',
-    'http://localhost:3000',
-    'http://localhost:5173'
+    'https://green-thumb13-fps41zcah-sudi67s-projects.vercel.app'
 ]
 
 db.init_app(app)
